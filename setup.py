a9546d617548c56a648bd7ddff6dc3279e54b6c2
Ryan Benmalek <ryanai3@hotmail.com>
2024-04-12 22:35:44 -0400

    UGHGHGHG

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 
 def load_requirements(filename='requirements.txt'):
-    standard_reqs = []
     with open(filename, 'r') as file:
+        requirements = []
         for line in file:
             line = line.strip()
             if line.startswith('#') or not line:
                 continue
             if '#' in line:
-                line = line[:line.index('#')].strip()
-            if line.startswith('git+') or line.startswith('svn+') or line.startswith('hg+'):
-                # These are VCS dependencies that cannot be processed here
                 continue
-            else:
-                standard_reqs.append(line)
-    return standard_reqs
+            if line.startswith('git+') or line.startswith('svn+') or line.startswith('hg+'):
+                continue  # Skip VCS dependencies
+            requirements.append(line)
+    return requirements
 
 requirements = load_requirements()
 
 setup(
     name='cartoonsegmentation',
     version='0.1.0',
-    packages=find_packages(),
-    install_requires=requirements
+    py_modules=['examples', 'animeinsseg', 'anime_3dkenburns', 'depth_modules', 'utils', 'Web_UI'],  # List individual module files here
+    scripts=['run_kenburns.py', 'run_style.py', 'naive_interface.py', 'repaint_person.py'],  # Scripts that should be accessible from the command line
+    install_requires=[
+        # Dependencies here, same as before
+    ]
 )
 
